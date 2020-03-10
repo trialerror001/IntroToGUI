2722296c8ab0b547820ca439816f97869362023b
trialerror001 <ferdian.aditya2302@gmail.com>
2020-03-10 22:08:09 +0700

    project fix

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,26 +29,26 @@
 		private void InitializeComponent()
 		{
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.txtName = new System.Windows.Forms.TextBox();
 			this.txtAddress = new System.Windows.Forms.TextBox();
+			this.txtName = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
 			this.listNegara = new System.Windows.Forms.ListBox();
 			this.rbMale = new System.Windows.Forms.RadioButton();
 			this.rbFemale = new System.Windows.Forms.RadioButton();
 			this.chkCoding1 = new System.Windows.Forms.CheckBox();
 			this.chkCoding2 = new System.Windows.Forms.CheckBox();
 			this.groupBox2 = new System.Windows.Forms.GroupBox();
-			this.label3 = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
-			this.label5 = new System.Windows.Forms.Label();
-			this.label6 = new System.Windows.Forms.Label();
-			this.label7 = new System.Windows.Forms.Label();
-			this.txtNameR = new System.Windows.Forms.TextBox();
-			this.txtAddressR = new System.Windows.Forms.TextBox();
-			this.txtCityR = new System.Windows.Forms.TextBox();
-			this.txtGenderR = new System.Windows.Forms.TextBox();
 			this.txtSkillR = new System.Windows.Forms.TextBox();
+			this.txtGenderR = new System.Windows.Forms.TextBox();
+			this.txtCityR = new System.Windows.Forms.TextBox();
+			this.txtAddressR = new System.Windows.Forms.TextBox();
+			this.txtNameR = new System.Windows.Forms.TextBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
 			this.btnSubmit = new System.Windows.Forms.Button();
 			this.groupBox1.SuspendLayout();
 			this.groupBox2.SuspendLayout();
@@ -67,14 +67,19 @@
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "User Detail";
 			// 
-			// label1
+			// txtAddress
 			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(13, 56);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(90, 32);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "Name";
+			this.txtAddress.Location = new System.Drawing.Point(190, 119);
+			this.txtAddress.Name = "txtAddress";
+			this.txtAddress.Size = new System.Drawing.Size(330, 38);
+			this.txtAddress.TabIndex = 3;
+			// 
+			// txtName
+			// 
+			this.txtName.Location = new System.Drawing.Point(190, 56);
+			this.txtName.Name = "txtName";
+			this.txtName.Size = new System.Drawing.Size(330, 38);
+			this.txtName.TabIndex = 2;
 			// 
 			// label2
 			// 
@@ -85,19 +90,14 @@
 			this.label2.TabIndex = 1;
 			this.label2.Text = "Address";
 			// 
-			// txtName
+			// label1
 			// 
-			this.txtName.Location = new System.Drawing.Point(190, 56);
-			this.txtName.Name = "txtName";
-			this.txtName.Size = new System.Drawing.Size(330, 38);
-			this.txtName.TabIndex = 2;
-			// 
-			// txtAddress
-			// 
-			this.txtAddress.Location = new System.Drawing.Point(190, 119);
-			this.txtAddress.Name = "txtAddress";
-			this.txtAddress.Size = new System.Drawing.Size(330, 38);
-			this.txtAddress.TabIndex = 3;
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 56);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(90, 32);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Name";
 			// 
 			// listNegara
 			// 
@@ -174,41 +174,40 @@
 			this.groupBox2.TabStop = false;
 			this.groupBox2.Text = "User Profile";
 			// 
-			// label3
+			// txtSkillR
 			// 
-			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(24, 62);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(90, 32);
-			this.label3.TabIndex = 0;
-			this.label3.Text = "Name";
+			this.txtSkillR.Location = new System.Drawing.Point(210, 317);
+			this.txtSkillR.Name = "txtSkillR";
+			this.txtSkillR.Size = new System.Drawing.Size(313, 38);
+			this.txtSkillR.TabIndex = 9;
 			// 
-			// label4
+			// txtGenderR
 			// 
-			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(24, 125);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(119, 32);
-			this.label4.TabIndex = 1;
-			this.label4.Text = "Address";
+			this.txtGenderR.Location = new System.Drawing.Point(210, 251);
+			this.txtGenderR.Name = "txtGenderR";
+			this.txtGenderR.Size = new System.Drawing.Size(313, 38);
+			this.txtGenderR.TabIndex = 8;
 			// 
-			// label5
+			// txtCityR
 			// 
-			this.label5.AutoSize = true;
-			this.label5.Location = new System.Drawing.Point(24, 189);
-			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(64, 32);
-			this.label5.TabIndex = 2;
-			this.label5.Text = "City";
+			this.txtCityR.Location = new System.Drawing.Point(210, 186);
+			this.txtCityR.Name = "txtCityR";
+			this.txtCityR.Size = new System.Drawing.Size(313, 38);
+			this.txtCityR.TabIndex = 7;
 			// 
-			// label6
+			// txtAddressR
 			// 
-			this.label6.AutoSize = true;
-			this.label6.Location = new System.Drawing.Point(24, 254);
-			this.label6.Name = "label6";
-			this.label6.Size = new System.Drawing.Size(110, 32);
-			this.label6.TabIndex = 3;
-			this.label6.Text = "Gender";
+			this.txtAddressR.Location = new System.Drawing.Point(210, 122);
+			this.txtAddressR.Name = "txtAddressR";
+			this.txtAddressR.Size = new System.Drawing.Size(313, 38);
+			this.txtAddressR.TabIndex = 6;
+			// 
+			// txtNameR
+			// 
+			this.txtNameR.Location = new System.Drawing.Point(210, 56);
+			this.txtNameR.Name = "txtNameR";
+			this.txtNameR.Size = new System.Drawing.Size(313, 38);
+			this.txtNameR.TabIndex = 5;
 			// 
 			// label7
 			// 
@@ -219,40 +218,41 @@
 			this.label7.TabIndex = 4;
 			this.label7.Text = "Skill";
 			// 
-			// txtNameR
+			// label6
 			// 
-			this.txtNameR.Location = new System.Drawing.Point(210, 56);
-			this.txtNameR.Name = "txtNameR";
-			this.txtNameR.Size = new System.Drawing.Size(313, 38);
-			this.txtNameR.TabIndex = 5;
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(24, 254);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(110, 32);
+			this.label6.TabIndex = 3;
+			this.label6.Text = "Gender";
 			// 
-			// txtAddressR
+			// label5
 			// 
-			this.txtAddressR.Location = new System.Drawing.Point(210, 122);
-			this.txtAddressR.Name = "txtAddressR";
-			this.txtAddressR.Size = new System.Drawing.Size(313, 38);
-			this.txtAddressR.TabIndex = 6;
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(24, 189);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(64, 32);
+			this.label5.TabIndex = 2;
+			this.label5.Text = "City";
 			// 
-			// txtCityR
+			// label4
 			// 
-			this.txtCityR.Location = new System.Drawing.Point(210, 186);
-			this.txtCityR.Name = "txtCityR";
-			this.txtCityR.Size = new System.Drawing.Size(313, 38);
-			this.txtCityR.TabIndex = 7;
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(24, 125);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(119, 32);
+			this.label4.TabIndex = 1;
+			this.label4.Text = "Address";
 			// 
-			// txtGenderR
+			// label3
 			// 
-			this.txtGenderR.Location = new System.Drawing.Point(210, 251);
-			this.txtGenderR.Name = "txtGenderR";
-			this.txtGenderR.Size = new System.Drawing.Size(313, 38);
-			this.txtGenderR.TabIndex = 8;
-			// 
-			// txtSkillR
-			// 
-			this.txtSkillR.Location = new System.Drawing.Point(210, 317);
-			this.txtSkillR.Name = "txtSkillR";
-			this.txtSkillR.Size = new System.Drawing.Size(313, 38);
-			this.txtSkillR.TabIndex = 9;
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(24, 62);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(90, 32);
+			this.label3.TabIndex = 0;
+			this.label3.Text = "Name";
 			// 
 			// btnSubmit
 			// 
@@ -262,6 +262,7 @@
 			this.btnSubmit.TabIndex = 7;
 			this.btnSubmit.Text = "Submit";
 			this.btnSubmit.UseVisualStyleBackColor = true;
+			this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
 			// 
 			// Form1
 			// 
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleProfile
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			txtNameR.Enabled = false;
+			txtAddressR.Enabled = false;
+			txtCityR.Enabled = false;
+			txtGenderR.Enabled = false;
+			txtSkillR.Enabled = false;
+		}
+
+		private void btnSubmit_Click(object sender, EventArgs e)
+		{
+			string list = listNegara.GetItemText(listNegara.SelectedItem);
+			string gender;
+			string skill = "";
+
+			if (rbMale.Checked)
+				gender = rbMale.Text;
+			else
+				gender = rbFemale.Text;
+
+			if (chkCoding1.Checked && chkCoding2.Checked)
+				skill = "C# & ASP";
+			else if (chkCoding1.Checked)
+				skill = "C#";
+			else if (chkCoding2.Checked)
+				skill = "ASP";
+			
+			txtNameR.Text = txtName.Text;
+			txtAddressR.Text = txtAddress.Text;
+			txtCityR.Text = list;
+			txtGenderR.Text = gender;
+			txtSkillR.Text = skill;
+
+		}
+	}
+}
